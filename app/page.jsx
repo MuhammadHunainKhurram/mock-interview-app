daaba6cebd4db7d7ff55f26aa2a76c3115e548bb
Muhammad <mhk8367@mavs.uta.edu>
2024-06-21 18:33:18 -0500

    Add Home Screen

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
+import Header from "./dashboard/_components/Header";
 
 export default function Home() {
   return (
-    <div>
-      <h2>Hello</h2>
+    <div className="min-h-screen bg-dark flex flex-col">
+      <div className="flex-grow flex items-center justify-center">
+            <Link href="/dashboard">
+              <button className="text-4xl text-center text-primary mb-8 font-bold hover:text-white">
+                Click Me
+              </button>
+            </Link>
+      </div>
     </div>
   );
 }
